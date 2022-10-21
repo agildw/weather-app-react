6f4f2956d460bc790ac83912576d4255f5f51cb3
Agil Dwiki Yudistira <yudistiraagil24@gmail.com>
2022-10-21 20:41:28 +0700

    add svg component

---
+++ src/components/svg/MountainSVG.js
@@ -0,0 +1,143 @@
+import * as React from "react";
+
+const MountainSVG = (props) => (
+  <svg
+    width={387}
+    height={314}
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M303.234 195.117C303.234 87.357 215.877 0 108.117 0 .357 0-87 87.357-87 195.117c0 107.76 87.357 195.117 195.117 195.117 107.76 0 195.117-87.357 195.117-195.117Z"
+      fill="url(#a)"
+    />
+    <path
+      d="M108.447 184.555c28.223 0 51.102-22.879 51.102-51.102 0-28.223-22.879-51.102-51.102-51.102-28.223 0-51.102 22.879-51.102 51.102 0 28.223 22.88 51.102 51.102 51.102Z"
+      fill="#FB6849"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M100.839 142.744 387 300.32v181.417h-548l2.59-174.907 259.249-164.086Z"
+      fill="#30243B"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="m96.901 155.343 16.099 3.94-1.241-10.453 88.63 48.984h-15.035l-6.354 4.68-35.743-20.182 27.473 26.272-7.172 5.282 4.419 12.012-31.895 14.407-32.403-32.105 20.848 3.8-13.201-20.46 31.931 14.846-27.017-24.054 12.163 4.1-31.501-31.069Z"
+      fill="url(#b)"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="m204.043 208.009 9.658-1.644 96.211 53.435-13.468 14.024-14.843-5.027 9.358 10.765-23.145 24.133-33.496-14.683 19.105 34.386-60.269-42.662-1.603-27.114-15.112-20.626 14.106 3.619-4.823-19.606 21.401-2.271-3.08-6.729Z"
+      fill="url(#c)"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="m77.147 238.692 30.31 16.128v-10.862l62.59 55.03-9.906-22.016 36.567 12.041 35.844 43.141-27.754 3.944-56.153-26.708 28.339 30.661-33.456 4.754-66.38-59.609v-18.951l19.754 10.727-19.755-38.28Z"
+      fill="url(#d)"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="m-21.907 243.863-2.768-14.635 31.252 11.057 31.25 25.458-31.2-3.273-28.534-18.607Z"
+      fill="url(#e)"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="m-6.996 284.506-10.058-16.519 25.306 5.939-4.07 6.699-11.178 3.881Z"
+      fill="url(#f)"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M5.04 217.799h9.913l-4.577-9.62 13.397 5.861 14.055 11.496-18.074 5.344L5.04 217.799Z"
+      fill="url(#g)"
+    />
+    <defs>
+      <linearGradient
+        id="a"
+        x1={-140.146}
+        y1={0}
+        x2={-140.146}
+        y2={496.526}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#231440" />
+        <stop offset={1} stopColor="#685AAB" />
+      </linearGradient>
+      <linearGradient
+        id="b"
+        x1={96.901}
+        y1={148.83}
+        x2={96.901}
+        y2={240.285}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#B6A5C4" />
+        <stop offset={1} stopColor="#31253B" />
+      </linearGradient>
+      <linearGradient
+        id="c"
+        x1={176.439}
+        y1={206.365}
+        x2={176.439}
+        y2={323.398}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#B6A5C4" />
+        <stop offset={1} stopColor="#31253B" />
+      </linearGradient>
+      <linearGradient
+        id="d"
+        x1={77.147}
+        y1={238.692}
+        x2={77.147}
+        y2={344.805}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#B6A5C4" />
+        <stop offset={1} stopColor="#31253B" />
+      </linearGradient>
+      <linearGradient
+        id="e"
+        x1={-24.675}
+        y1={229.228}
+        x2={-24.675}
+        y2={265.743}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#B6A5C4" />
+        <stop offset={1} stopColor="#31253B" />
+      </linearGradient>
+      <linearGradient
+        id="f"
+        x1={-17.054}
+        y1={267.987}
+        x2={-17.054}
+        y2={284.506}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#B6A5C4" />
+        <stop offset={1} stopColor="#31253B" />
+      </linearGradient>
+      <linearGradient
+        id="g"
+        x1={5.039}
+        y1={208.179}
+        x2={5.039}
+        y2={230.88}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#B6A5C4" />
+        <stop offset={1} stopColor="#31253B" />
+      </linearGradient>
+    </defs>
+  </svg>
+);
+
+export default MountainSVG;
