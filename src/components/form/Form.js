0dcecdf0203b13ff08c43d21937d15d66be5c28a
Agil Dwiki Yudistira <yudistiraagil24@gmail.com>
2022-10-23 21:38:20 +0700

    add handling state

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,9 +1,36 @@
+import { useState } from "react";
 import FormComponent from "./FormComponent";
+import ResultComponent from "./ResultComponent";
 
 const Form = () => {
+  const [resultState, setResultState] = useState({
+    status: false,
+    weather: "",
+    description: "",
+    icon: "",
+    temp: "",
+    city: "",
+    humidity: "",
+    feelLike: "",
+  });
+  const handleResultState = (result) => {
+    console.log(result);
+    setResultState(result);
+  };
   return (
     <div className="flex items-center justify-center h-screen">
-      <FormComponent />
+      {!resultState.status && (
+        <FormComponent
+          resultState={resultState}
+          handleResultState={handleResultState}
+        />
+      )}
+      {resultState.status && (
+        <ResultComponent
+          resultState={resultState}
+          handleResultState={handleResultState}
+        />
+      )}
     </div>
   );
 };
